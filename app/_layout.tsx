61658efaa882755c5fcc111b2ee67c47afc9055f
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-31 22:19:57 +0500

    add(RootLayout): updated with native tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,8 @@
+import { COLORS } from '@/utils/Colors';
 import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
 
 export default function RootLayout() {
-  return <NativeTabs>
+  return <NativeTabs tintColor={COLORS.textDark}>
       <NativeTabs.Trigger name="index">
         <Label>Home</Label>
         <Icon sf="house.fill" drawable="custom_android_drawable" />
