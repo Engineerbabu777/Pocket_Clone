ae7bc5eaad530ff0ddd6c27078fff594271aaeb6
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-31 22:29:04 +0500

    add(RootLayout): updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,18 +2,21 @@ import { COLORS } from '@/utils/Colors';
 import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
 
 export default function RootLayout() {
-  return <NativeTabs tintColor={COLORS.textDark}>
+  return  <NativeTabs blurEffect="systemChromeMaterial" tintColor={COLORS.textDark}>
       <NativeTabs.Trigger name="home">
         <Label>Home</Label>
-        <Icon sf="house.fill" drawable="custom_android_drawable" />
+        <Icon sf={{ default: 'house', selected: 'house.fill' }} drawable="home_drawable" />
       </NativeTabs.Trigger>
-       <NativeTabs.Trigger name="saves">
-        <Icon sf="steeringwheel.and.heat.waves" drawable="custom_settings_drawable" />
+      <NativeTabs.Trigger name="saves">
         <Label>Saves</Label>
+        <Icon sf={{ default: 'heart', selected: 'heart.fill' }} drawable="star_drawable" />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="settings">
-        <Icon sf="gear" drawable="custom_settings_drawable" />
         <Label>Settings</Label>
+        <Icon
+          sf={{ default: 'gearshape', selected: 'gearshape.fill' }}
+          drawable="settings_drawable"
+        />
       </NativeTabs.Trigger>
     </NativeTabs>;
 }
