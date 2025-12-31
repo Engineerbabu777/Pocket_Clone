2df0c901fb5e9cd9eb6be85eb000d1e13b3016a5
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-31 23:44:04 +0500

    add(Layout): updated a bit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,22 @@
 
 
 
-import { Stack } from 'expo-router'
-import React from 'react'
-import { KeyboardProvider } from 'react-native-keyboard-controller'
-
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
+import { Stack } from 'expo-router';
+import React from 'react';
+import { KeyboardProvider } from 'react-native-keyboard-controller';
 
 const Layout = () => {
   return (
     <KeyboardProvider>
+          <ClerkProvider tokenCache={tokenCache}>
+
     
     <Stack>
         <Stack.Screen name='index' options={{ headerShown:false }} />
     </Stack>
+          </ClerkProvider>
 
     </KeyboardProvider>
 
