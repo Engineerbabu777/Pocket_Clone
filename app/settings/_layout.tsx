a07fe8d9e627ec001a01cd283123b3a5d6863894
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-31 22:24:06 +0500

    add(SettingsLayout): new layout has been added nd updated

--- app/settings/_layout.tsx
+++ app/settings/_layout.tsx
@@ -2,5 +2,6 @@ import { Stack } from 'expo-router';
 
 export default function SettingsLayout() {
   return <Stack >
+    <Stack.Screen name="index" options={{ title: "Home", headerShown: false }} />
 </Stack>;
 }
