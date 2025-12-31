19dc37adb9b3d28667db76001163c2cd3d53605c
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-31 22:27:07 +0500

    add(HomeLayout): this has been udpated

--- app/home/_layout.tsx
+++ app/home/_layout.tsx
@@ -1,7 +1,22 @@
+import { COLORS } from '@/utils/Colors';
 import { Stack } from 'expo-router';
 
 export default function HomeLayout() {
-  return <Stack >
-    <Stack.Screen name="index" options={{ title: "Home", headerShown: false }} />
-</Stack>;
+  return (
+    <Stack
+      screenOptions={{
+        contentStyle: {
+          backgroundColor: COLORS.white,
+        },
+      }}>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          headerLargeTitle: true,
+          headerLargeTitleShadowVisible: false,
+        }}
+      />
+    </Stack>
+  );
 }
