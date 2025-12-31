66084a1bc9437c9c3311fb3aad9259a07259ea83
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-31 22:27:46 +0500

    add(SavesLayout): updated a bit

--- app/saves/_layout.tsx
+++ app/saves/_layout.tsx
@@ -1,7 +1,22 @@
+import { COLORS } from '@/utils/Colors';
 import { Stack } from 'expo-router';
 
 export default function SavesLayout() {
-  return <Stack >
-    <Stack.Screen name="index" options={{ title: "Saves", headerShown: false }} />
-</Stack>;
+  return (
+    <Stack
+      screenOptions={{
+        contentStyle: {
+          backgroundColor: COLORS.white,
+        },
+      }}>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: 'Saves',
+          headerLargeTitle: true,
+          headerLargeTitleShadowVisible: false,
+        }}
+      />
+    </Stack>
+  );
 }
