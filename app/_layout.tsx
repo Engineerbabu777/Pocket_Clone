097316d504acecdd84fcf9ac39be65aa3a8bfb18
EngineerBabu777 <awaismumtaz0099@gmail.com>
2025-12-31 22:26:08 +0500

    add(RootLayout): udapted a bit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,9 +3,13 @@ import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
 
 export default function RootLayout() {
   return <NativeTabs tintColor={COLORS.textDark}>
-      <NativeTabs.Trigger name="index">
+      <NativeTabs.Trigger name="home">
         <Label>Home</Label>
         <Icon sf="house.fill" drawable="custom_android_drawable" />
+      </NativeTabs.Trigger>
+       <NativeTabs.Trigger name="saves">
+        <Icon sf="steeringwheel.and.heat.waves" drawable="custom_settings_drawable" />
+        <Label>Saves</Label>
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="settings">
         <Icon sf="gear" drawable="custom_settings_drawable" />
