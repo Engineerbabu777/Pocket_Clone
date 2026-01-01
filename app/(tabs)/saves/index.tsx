52c9370735abd198012994729d338d361093c357
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-01 12:17:43 +0500

    add(styles): styles added for saves screen

--- app/(tabs)/saves/index.tsx
+++ app/(tabs)/saves/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { COLORS } from "@/utils/Colors";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function SavesScreen() {
   return (
@@ -13,3 +14,41 @@ export default function SavesScreen() {
     </View>
   );
 }
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+  },
+  listContent: {
+    paddingVertical: 10,
+    backgroundColor: COLORS.white,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#f0f0f0',
+    marginStart: 20,
+  },
+  emptyState: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 40,
+  },
+  emptyIcon: {
+    marginBottom: 20,
+  },
+  emptyTitle: {
+    fontSize: 22,
+    fontWeight: '600',
+    color: COLORS.textDark,
+    marginBottom: 8,
+  },
+  emptyDescription: {
+    fontSize: 16,
+    color: COLORS.textGray,
+    textAlign: 'center',
+  },
+});
