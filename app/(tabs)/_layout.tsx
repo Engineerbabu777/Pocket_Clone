3228cb57e781f8bd6208f4ff746ca48c5acbe915
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-01 16:23:58 +0500

    add(TabRootLayout): this has been updated a bit for purchaeses

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,14 @@
 import { COLORS } from '@/utils/Colors';
 import { Platform } from 'react-native';
 
-import { Tabs } from 'expo-router';
+import { Tabs, useRouter } from 'expo-router';
 import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
 
+import { useUser } from '@clerk/clerk-expo';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import * as Sentry from '@sentry/react-native';
+import { useEffect } from 'react';
+import Purchases, { LOG_LEVEL } from 'react-native-purchases';
 
 function IosTabsLayout() {
   return (
@@ -67,7 +71,39 @@ function AndroidTabsLayout() {
   );
 }
 
-export default function RootLayout() {
+export default function TabRootLayout() {
+
+   const { hasShareIntent, shareIntent, resetShareIntent, error } = useShareIntentContext();
+  const router = useRouter();
+  const user = useUser();
+
+
+    useEffect(() => {
+    if (hasShareIntent && shareIntent.type === 'weburl' && shareIntent.webUrl) {
+      router.push(`/(modal)/add-url?intent=${encodeURIComponent(shareIntent?.webUrl ?? '')}`);
+      resetShareIntent();
+    }
+  }, [hasShareIntent]);
+
+  useEffect(() => {
+    Purchases.setLogLevel(LOG_LEVEL.VERBOSE);
+
+    if (Platform.OS === 'ios') {
+      Purchases.configure({ apiKey: process.env.EXPO_PUBLIC_RC_APPLE_KEY! });
+    } else if (Platform.OS === 'android') {
+      Purchases.configure({ apiKey: process.env.EXPO_PUBLIC_REVENUECAT_PROJECT_GOOGLE_API_KEY! });
+    }
+  }, []);
+
+  useEffect(() => {
+    if (user && user.user) {
+      Sentry.setUser({ email: user.user.emailAddresses[0].emailAddress, id: user.user.id });
+      Purchases.logIn(user.user.id);
+    } else {
+      Sentry.setUser(null);
+    }
+  }, [user]);
+
   if (Platform.OS === 'ios') {
     return <IosTabsLayout />;
   } else {
