def1e6989783c61dfc44d01c8434f6771fe2d89e
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-01 12:20:11 +0500

    add(SavesScreen): this has been updated with loadmore items method

--- app/(tabs)/saves/index.tsx
+++ app/(tabs)/saves/index.tsx
@@ -1,7 +1,51 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { SavedItem, savedItems } from "@/db/schema";
 import { COLORS } from "@/utils/Colors";
+import { useUser } from "@clerk/clerk-expo";
+import { and, desc, eq, or } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/expo-sqlite";
+import { useFocusEffect } from "expo-router";
+import { useSQLiteContext } from "expo-sqlite";
+import { useCallback, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function SavesScreen() {
+
+  const [items, setItems] = useState<SavedItem[]>([]);
+  const [loading, setLoading] = useState(true);
+  const { user } = useUser();
+
+  const db = useSQLiteContext();
+  const drizzleDb = drizzle(db);
+
+
+  const loadSavedItems = async () => {
+    try {
+      const result = await drizzleDb
+        .select()
+        .from(savedItems)
+        .where(
+          and(
+            eq(savedItems.is_deleted, false),
+            or(eq(savedItems.user_id, user?.id || ''), eq(savedItems.user_id, '1'))
+          )
+        )
+        .orderBy(desc(savedItems.created_at));
+
+      setItems(result);
+    } catch (error) {
+      console.error('Failed to load saved items:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadSavedItems();
+    }, [loadSavedItems])
+  );
+
   return (
     <View
       style={{
