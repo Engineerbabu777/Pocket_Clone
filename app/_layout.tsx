baf338b5750fffe7af0f3eae90dd9ceff2ae696b
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-01 09:20:28 +0500

    add(RootLayout): this has been updated

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,46 @@
-
-
-
-import { ClerkProvider } from '@clerk/clerk-expo';
+import { ClerkLoaded, ClerkProvider, useAuth } from '@clerk/clerk-expo';
 import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Stack } from 'expo-router';
-import React from 'react';
+import { Suspense } from 'react';
+import { ActivityIndicator } from 'react-native';
 import { KeyboardProvider } from 'react-native-keyboard-controller';
 
-const Layout = () => {
-    return (
-        <KeyboardProvider>
-            <ClerkProvider tokenCache={tokenCache}>
-                <Stack>
-                    <Stack.Screen name='index' options={{ headerShown: false }} />
-                </Stack>
-            </ClerkProvider>
 
-        </KeyboardProvider>
 
-    )
-}
 
-export default Layout
+
+
+const RootNav = () => {
+
+  const { isSignedIn } = useAuth();
+
+  return (
+    <Stack>
+      <Stack.Protected guard={!isSignedIn}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack.Protected>
+
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    
+    </Stack>
+  );
+};
+
+const RootLayout = () => {
+
+  return (
+  
+      <ClerkProvider tokenCache={tokenCache}>
+        <ClerkLoaded>
+          <KeyboardProvider>
+            <Suspense fallback={<ActivityIndicator />}>
+           
+                <RootNav />
+            </Suspense>
+          </KeyboardProvider>
+        </ClerkLoaded>
+      </ClerkProvider>
+  );
+};
+
+export default RootLayout;
