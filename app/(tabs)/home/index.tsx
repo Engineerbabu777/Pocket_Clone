9b1cae2b2d9c0e24bd727bdf62577d244cbc5acb
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-01 09:30:08 +0500

    add(styles): this has been added for home index

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { COLORS } from "@/utils/Colors";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
@@ -13,3 +14,54 @@ export default function HomeScreen() {
     </View>
   );
 }
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+  },
+  content: {
+    flex: 1,
+    paddingHorizontal: 30,
+  },
+  loadingContainer: {
+    alignItems: 'center',
+    paddingVertical: 60,
+  },
+  loadingShapes: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 20,
+    marginBottom: 20,
+  },
+  shape: {
+    width: 40,
+    height: 40,
+  },
+  circle: {
+    backgroundColor: '#E85A4F',
+    borderRadius: 20,
+  },
+  triangle: {
+    width: 0,
+    height: 0,
+    backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderLeftWidth: 20,
+    borderRightWidth: 20,
+    borderBottomWidth: 35,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: '#F4A261',
+  },
+  square: {
+    backgroundColor: '#2A9D8F',
+  },
+  loadingText: {
+    fontSize: 18,
+    color: COLORS.textGray,
+    fontWeight: '500',
+  },
+});
